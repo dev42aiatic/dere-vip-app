19d7f175f3066b338e3b4dbe557338d9d3cc408f
Francisco Gonzalez <dev42@aiatic.com>
2023-12-14 18:52:57 -0500

    Initial lightColorScheme created

---
+++ lib/core/theme/color_schemes.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+
+  // Color principal, utilizado en elementos como la barra de navegación y botones.
+  primary: Color(0xFF212C3A),
+
+  // Color para texto e iconos sobre un fondo primary.
+  onPrimary: Color(0xFFFFFFFF),
+
+  // Variante del color principal para contenedores más grandes.
+  primaryContainer: Color(0xFFF2F2F2),
+
+  // Color para texto e iconos sobre primaryContainer.
+  onPrimaryContainer: Color(0xFF141A22),
+
+  // Color secundario, para acentuar elementos menos prominentes que los primarios.
+  secondary: Color(0xFF9C7A4F),
+
+  // Color para texto e iconos sobre un fondo secondary.
+  onSecondary: Color(0xFFFFFFFF),
+
+  // Variante del color secundario para contenedores.
+  secondaryContainer: Color(0xFFF2F2F2),
+
+  // Color para texto e iconos sobre secondaryContainer.
+  onSecondaryContainer: Color(0xFF2B151C),
+
+  // Tercer color, utilizado para acentuar otros elementos distintos.
+  tertiary: Color(0xFF9C7A4F),
+
+  // Color para texto e iconos sobre un fondo tertiary.
+  onTertiary: Color(0xFFFFFFFF),
+
+  // Variante de tertiary para contenedores.
+  tertiaryContainer: Color(0xFFF2F2F2),
+
+  // Color para texto e iconos sobre tertiaryContainer.
+  onTertiaryContainer: Color(0xFF2B151C),
+
+  // Color utilizado para señalar errores.
+  error: Color(0xFFBA1A1A),
+
+  // Variante de color para contenedores de errores.
+  errorContainer: Color(0xFFFFDAD6),
+
+  // Color para texto e iconos sobre un fondo de error.
+  onError: Color(0xFFFFFFFF),
+
+  // Color para texto e iconos sobre errorContainer.
+  onErrorContainer: Color(0xFF410002),
+
+  // Color de fondo general de la aplicación.
+  background: Color(0xFFFFFBFF),
+
+  // Color para texto e iconos sobre el fondo general.
+  onBackground: Color(0xFF201A1B),
+
+  // Color para superficies de tarjetas, diálogos, etc.
+  surface: Color(0xFFFFFBFF),
+
+  // Color para texto e iconos sobre surface.
+  onSurface: Color(0xFF201A1B),
+
+  // Variante de surface para diferenciación visual.
+  surfaceVariant: Color(0xFFF2DDE1),
+
+  // Color para texto e iconos sobre surfaceVariant.
+  onSurfaceVariant: Color(0xFF514347),
+
+  // Color utilizado para bordes y divisiones.
+  outline: Color(0xFF837377),
+
+  // Color para texto e iconos en una superficie inversa.
+  onInverseSurface: Color(0xFFFAEEEF),
+
+  // Color para superficies con colores invertidos.
+  inverseSurface: Color(0xFF352F30),
+
+  // Variante inversa del color primario.
+  inversePrimary: Color(0xFFFFB1C8),
+
+  // Utilizado para sombras y efectos de elevación.
+  shadow: Color(0xFF000000),
+
+  // Tonalidad aplicada a algunas superficies como un ligero tinte de color.
+  surfaceTint: Color(0xFFB90063),
+
+  // Otra opción para bordes y divisiones.
+  outlineVariant: Color(0xFFD5C2C6),
+
+  // Color para velos y superposiciones.
+  scrim: Color(0xFF000000),
+);

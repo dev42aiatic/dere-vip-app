401728a339ecd6d01f6086161eaf44852d006ba7
Francisco Gonzalez <dev42@aiatic.com>
2023-12-15 07:23:29 -0500

    textTheme implemented in ThemeData

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,6 +1,7 @@
 // Importación selectiva de las clases Color y ThemeData desde la biblioteca flutter/material.
 import 'package:dere_vip_app/core/theme/color_schemes.dart';
-import 'package:flutter/material.dart' show ThemeData;
+import 'package:flutter/material.dart' show FontWeight, ThemeData, TextTheme;
+import 'package:google_fonts/google_fonts.dart';
 
 // Definición de la clase AppTheme.
 class AppTheme {
@@ -13,5 +14,29 @@ class AppTheme {
         useMaterial3: true,
         // Establece el esquema de colores de la aplicación.
         colorScheme: lightColorScheme,
+
+        ///* Texts
+        textTheme: TextTheme(
+          bodyLarge: GoogleFonts.montserratAlternates().copyWith(
+            fontSize: 14,
+          ),
+          bodyMedium: GoogleFonts.montserratAlternates().copyWith(
+            fontSize: 12,
+          ),
+          bodySmall: GoogleFonts.montserratAlternates().copyWith(
+            fontSize: 10,
+          ),
+          titleLarge: GoogleFonts.montserratAlternates().copyWith(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          titleMedium: GoogleFonts.montserratAlternates().copyWith(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          titleSmall: GoogleFonts.montserratAlternates().copyWith(
+            fontSize: 12,
+          ),
+        ),
       );
 }

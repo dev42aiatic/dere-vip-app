eb64f42a9aa36e9aa8efa34589af477106836cd4
Francisco Gonzalez <dev42@aiatic.com>
2023-12-16 15:11:08 -0500

    GestureDetector added to HighCourtButton

--- lib/src/high_court/presetantion/widgets/high_court_button.dart
+++ lib/src/high_court/presetantion/widgets/high_court_button.dart
@@ -23,37 +23,43 @@ class HighCourtButton extends StatelessWidget {
     // Accediendo al esquema de color actual del contexto de este widget.
     final colors = Theme.of(context).colorScheme;
 
-    return Container(
-      // Margen inferior para separación
-      margin: const EdgeInsets.only(bottom: 18),
-      height: 50, // Altura fija del botón
-      width: 300, // Anchura fija del botón
-      // Color primario y border redondeados del botón
-      decoration: BoxDecoration(
-        color: colors.primary,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Stack(
-        children: [
-          Center(
-            // Centrando el texto en el botón
-            child: Text(
-              text,
-              style: TextStyle(
-                color: colors.onSecondary,
-              ), // Color del texto basado en el esquema de colores
+    return GestureDetector(
+      onTap: () {
+        //TODO: Implementar variable para la ruta deseada. Se debe pasar desde el constructor.
+      },
+      child: Container(
+        // Margen inferior para separación
+        margin: const EdgeInsets.only(bottom: 18),
+        height: 50, // Altura fija del botón
+        width: 300, // Anchura fija del botón
+        // Color primario y border redondeados del botón
+        decoration: BoxDecoration(
+          color: colors.primary,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Stack(
+          children: [
+            Center(
+              // Centrando el texto en el botón
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: colors.onSecondary,
+                ), // Color del texto basado en el esquema de colores
+              ),
             ),
-          ),
-          Positioned(
-            right: 5, // Posiciona el ícono a la derecha con un margen de 5
-            top: 0, // Alineado con la parte superior del botón
-            bottom: 0, // Alineado con la parte inferior del botón
-            child: Icon(
-              Icons.arrow_right, // Ícono de flecha a la derecha
-              color: colors.tertiary, // Color terciario del tema para el ícono
+            Positioned(
+              right: 5, // Posiciona el ícono a la derecha con un margen de 5
+              top: 0, // Alineado con la parte superior del botón
+              bottom: 0, // Alineado con la parte inferior del botón
+              child: Icon(
+                Icons.arrow_right, // Ícono de flecha a la derecha
+                color:
+                    colors.tertiary, // Color terciario del tema para el ícono
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
